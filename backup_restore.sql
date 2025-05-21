46fd9aa3a0a7bded66cce65c8855bcd86148d858
PHILIP THOMAS <thomasphil6889@gmail.com>
2025-05-21 17:23:25 +0530

    Create backup_restore.sql

---
+++ backup_restore.sql
@@ -0,0 +1,6 @@
+-- Backup (can be exported from tools like phpMyAdmin or MySQL Workbench)
+-- Restore via terminal:
+-- mysql -u username -p database_name < backup.sql
+
+-- Example: Export command
+-- mysqldump -u root -p Company > company_backup.sql
